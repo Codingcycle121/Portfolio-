cea7042ea8cf05ae4802f18785b4a8446122b2a7
Parag Bansal <110728211+Codingcycle121@users.noreply.github.com>
2024-07-17 14:44:25 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,127 @@
+var typed = new Typed(".multiple-text",{
+  strings:["ITM Gwalior"],
+  typeSpeed:100,
+  backSpeed:100,
+  backDelay:1000,
+  loop:true
+}
+)
+
+function glow1()
+{
+  
+
+
+   let home=document.getElementById("1");
+   home.style.color= "#fff";
+
+  
+    {
+      let about=document.getElementById("2");
+      if(about)i=2
+      if(i==2)
+      about.style.color= "#0ef";
+      let skills=document.getElementById("3");
+      skills.style.color= "#fff";
+   
+      let portfolio=document.getElementById("4");
+      portfolio.style.color= "#fff";
+   
+      let contact=document.getElementById("5");
+      contact.style.color= "#fff";
+   
+   }
+   
+  }
+  function glow2()
+{
+  
+
+
+   let home=document.getElementById("1");
+   home.style.color= "#fff";
+
+    {
+      let skills=document.getElementById("3");
+      if(skills)i=3
+      if(i==3)
+     skills.style.color= "#0ef";
+      let about=document.getElementById("2");
+      about.style.color= "#fff";
+   
+      let portfolio=document.getElementById("4");
+      portfolio.style.color= "#fff";
+   
+      let contact=document.getElementById("5");
+      contact.style.color= "#fff";
+   }
+
+}
+function glow3()
+{
+  
+
+
+   let home=document.getElementById("1");
+   home.style.color= "#fff";
+
+   {
+     let portfolio=document.getElementById("4");
+     if(portfolio)i=4
+      if(i==4)
+     portfolio.style.color= "#0ef";
+     let about=document.getElementById("2");
+   about.style.color= "#fff";
+
+   let skills=document.getElementById("3");
+   skills.style.color= "#fff";
+
+}
+}
+
+
+   function glow4()
+{
+  
+
+
+   let home=document.getElementById("1");
+   home.style.color= "#fff";
+
+
+    {
+      let contact=document.getElementById("5");
+      if(contact)i=5
+      if(i==5)
+      contact.style.color= "#0ef";
+      
+   let about=document.getElementById("2");
+   about.style.color= "#fff";
+
+   let skills=document.getElementById("3");
+   skills.style.color= "#fff";
+
+   let portfolio=document.getElementById("4");
+   portfolio.style.color= "#fff";
+
+    }
+   
+}
+
+function againGlow()
+{
+  let home=document.getElementById("1");
+   home.style.color= "#0ef";
+
+   let about=document.getElementById("2");
+   about.style.color= "#fff";
+
+   let skills=document.getElementById("3");
+   skills.style.color= "#fff";
+
+   let portfolio=document.getElementById("4");
+   portfolio.style.color= "#fff";
+
+   let contact=document.getElementById("5");
+   contact.style.color= "#fff";
+}
